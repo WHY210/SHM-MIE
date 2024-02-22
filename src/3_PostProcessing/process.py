53b4b8991acfa169be5779e97fc368eaf7127429
WHY210 <dulcinea1024@gmail.com>
2024-02-22 20:08:34 +0800

    refactor: process.py

--- src/3_PostProcessing/process.py
+++ src/3_PostProcessing/process.py
@@ -11,18 +11,15 @@ class DataProcessor:
         self.building_type = building_type
         self.path_MIE =  f'..\\..\\result\\{building_type}\\MIE'
         self.path_DI =  f'..\\..\\result\\{building_type}\\DI'
-        self.path_mean = f'..\\..\\result\\{building_type}\\MM'
+        self.path_MM = f'..\\..\\result\\{building_type}\\MM'
+        self.path_Minus_MIE = f'..\\..\\result\\{building_type}\\MIE_Minus'
+        self.path_Minus_DI = f'..\\..\\result\\{building_type}\\DI_Minus'
+        self.path_Minus_MM = f'..\\..\\result\\{building_type}\\MM_Minus'
         self.channel = channel
         self.scale = scale
 
-    def path(self):
-        os.makedirs(f'..\\..\\result\\{self.building_type}\\MIE', exist_ok=True)
-        os.makedirs(f'..\\..\\result\\{self.building_type}\\DI', exist_ok=True)
-        os.makedirs(f'..\\..\\result\\{self.building_type}\\MM', exist_ok=True)
-        print("finish path")
-
     def calculate_MinusMean(self):
-        os.makedirs(fr'{self.path_mean}', exist_ok=True)
+        os.makedirs(fr'{self.path_MM}', exist_ok=True)
 
         ## MEAN ##
         average_matrix = np.zeros((self.channel, self.scale))
@@ -30,14 +27,14 @@ class DataProcessor:
             matrix = np.loadtxt(f'{self.path_MIE}\\{file}')
             average_matrix += matrix
         average_matrix /= (i+1)
-        np.savetxt(fr'{self.path_mean}\MEAN.txt', average_matrix, fmt='%f', delimiter=' ')
+        np.savetxt(fr'{self.path_MM}\mean.txt', average_matrix, fmt='%f', delimiter=' ')
 
         ## MINUS ##
         for i, file in enumerate(os.listdir(self.path_MIE)):
             matrix = np.loadtxt(f'{self.path_MIE}\\{file}')
             matrix -= average_matrix
-            np.savetxt(fr'{self.path_mean}\MinusMean_{file}', matrix, fmt='%f', delimiter=' ')
-        print("finish calculate MinusMean")
+            np.savetxt(fr'{self.path_MM}\MM_{file}', matrix, fmt='%f', delimiter=' ')
+        print("finish calculate MM")
 
     def calculate_DI(self):
         os.makedirs(fr'{self.path_DI}', exist_ok=True)
@@ -47,13 +44,13 @@ class DataProcessor:
             ## REFERENCE ##
             if "七層鋼構架樓層破壞" in self.building_type:
 
-                UD001 = np.loadtxt(self.path_MIE + r'.\Mie_UD001.txt')
-                UD01 = np.loadtxt(self.path_MIE + r'.\Mie_UD01.txt')
-                UD002 = np.loadtxt(self.path_MIE + r'.\Mie_UD002.txt')
-                UD02 = np.loadtxt(self.path_MIE + r'.\Mie_UD02.txt')
-                UD003 = np.loadtxt(self.path_MIE + r'.\Mie_UD003.txt')
-                UD03 = np.loadtxt(self.path_MIE + r'.\Mie_UD03.txt')
-                UD04 = np.loadtxt(self.path_MIE + r'.\Mie_UD04.txt')
+                UD001 = np.loadtxt(self.path_MIE + r'\UD001.txt')
+                UD01 = np.loadtxt(self.path_MIE + r'\UD01.txt')
+                UD002 = np.loadtxt(self.path_MIE + r'\UD002.txt')
+                UD02 = np.loadtxt(self.path_MIE + r'\UD02.txt')
+                UD003 = np.loadtxt(self.path_MIE + r'\UD003.txt')
+                UD03 = np.loadtxt(self.path_MIE + r'\UD03.txt')
+                UD04 = np.loadtxt(self.path_MIE + r'\UD04.txt')
 
                 reference = (UD001 + UD01 + UD002 + UD02 + UD003 + UD03 + UD04) / 7
 
@@ -73,27 +70,16 @@ class DataProcessor:
 
     def scatter_plot(self, type, bottom, top):
         
-         
-        if type == "MIE":
-            path = self.path_MIE
-            os.makedirs(fr'{path}_scatterplot', exist_ok=True)
-            ylabel = "MIE (haven't summed up)"
-            channel = self.channel
-        elif type == "DI":
-            path = self.path_DI
-            os.makedirs(fr'{path}_scatterplot', exist_ok=True)
-            ylabel = "DI (haven't summed up)"
-            channel = self.channel
-        elif type == "Mean":
-            path = self.path_mean
-            os.makedirs(fr'{path}_scatterplot', exist_ok=True)
-            ylabel = "Minus mean (haven't summed up)"
-            channel = self.channel
-        elif type == "Minus":
-            path = self.path_minus
-            os.makedirs(fr'{path}_scatterplot', exist_ok=True)
-            ylabel = "Minus (haven't summed up)"
+        path = getattr(self, f'path_{type}')
+        os.makedirs(fr'{path}_scatterplot', exist_ok=True)
+
+        if type in ['Minus_MIE', 'Minus_DI', 'Minus_MM']:
             channel = self.channel - 1
+            ylabel = f"{type} (haven't summed up)"
+        else:
+            channel = self.channel
+            ylabel = f"{type} (haven't summed up)"
+
 
         for i, file in enumerate(os.listdir(path)):
 
@@ -105,7 +91,7 @@ class DataProcessor:
             plt.figure(figsize=(8, 4), dpi=150)
 
             for i in range(1, channel+1):
-                plt.plot(np.arange(1, self.scale + 1), data_matrix[i - 1, :], label=f"{i}", marker='o', markersize='3')
+                plt.plot(np.arange(1, self.scale + 1), data_matrix[i - 1, :], label=f"{i-1}", marker='o', markersize='3')
 
             # 設置標籤
             plt.xlabel("Scale", fontsize=8)
@@ -123,23 +109,15 @@ class DataProcessor:
         print(f'finish scatter - {type}')
 
     def sum_up(self, type):
-         
-        if type == "MIE":
-            path = self.path_MIE
-            os.makedirs(fr'{self.path_MIE}_SummedUp', exist_ok=True)
-            channel = self.channel
-        elif type == "DI":
-            path = self.path_DI
-            os.makedirs(fr'{self.path_DI}_SummedUp', exist_ok=True)
-            channel = self.channel
-        elif type == "Mean":
-            path = self.path_mean
-            os.makedirs(fr'{self.path_mean}_SummedUp', exist_ok=True)
-            channel = self.channel
-        elif type == "Minus":
-            path = self.path_minus
-            os.makedirs(fr'{self.path_minus}_SummedUp', exist_ok=True)
+
+        path = getattr(self, f'path_{type}')
+        os.makedirs(fr'{path}_SummedUp', exist_ok=True)
+
+        if type in ['Minus_MIE', 'Minus_DI', 'Minus_MM']:
             channel = self.channel - 1
+        else:
+            channel = self.channel
+
 
         for i, file in enumerate(os.listdir(path)):
 
@@ -157,19 +135,11 @@ class DataProcessor:
         print(f'finish sum up - {type}')
 
     def minus(self, type):
-         
-        if type == "MIE":
-            path = self.path_MIE
-            os.makedirs(fr'{path}_minus', exist_ok=True)
-        elif type == "DI":
-            path = self.path_DI
-            os.makedirs(fr'{path}_minus', exist_ok=True)
-        elif type == "Mean":
-            path = self.path_mean
-            os.makedirs(fr'{path}_minus', exist_ok=True)
 
-        self.path_minus = f'{path}_minus'
-
+        
+        path = getattr(self, f'path_{type}')
+        os.makedirs(f'{path}_Minus', exist_ok=True)
+        
         for i, file in enumerate(os.listdir(path)):
 
             data = np.loadtxt(f'{path}\\{file}')
@@ -181,32 +151,21 @@ class DataProcessor:
             data_matrix = np.diff(data_matrix, axis=0)
 
             # 將相加後的數據保存為txt檔
-            np.savetxt(f'{path}_minus\\{file}', data_matrix)
+            np.savetxt(f'{path}_Minus\\{file}', data_matrix, fmt='%.6f')
         
         print(f'finish calculate minus - {type}')
 
     def histogram(self, type, bottom, top):
         
-        if type == "MIE":
-            path = self.path_MIE
-            os.makedirs(fr'{path}_histogram', exist_ok=True)
-            ylabel = "MIE (summed up)"
-            channel = self.channel
-        elif type == "DI":
-            path = self.path_DI
-            os.makedirs(fr'{path}_histogram', exist_ok=True)
-            ylabel = "DI (summed up)"
-            channel = self.channel
-        elif type == "Mean":
-            path = self.path_mean
-            os.makedirs(fr'{path}_histogram', exist_ok=True)
-            ylabel = "Minus mean (summed up)"
-            channel = self.channel
-        elif type == "Minus":
-            path = self.path_minus
-            os.makedirs(fr'{path}_histogram', exist_ok=True)
-            ylabel = "Minus (summed up)"
+        path = getattr(self, f'path_{type}')
+        os.makedirs(fr'{path}_histogram', exist_ok=True)
+
+        if type in ['Minus_MIE', 'Minus_DI', 'Minus_MM']:
             channel = self.channel - 1
+            ylabel = f'{type} (summed up)'
+        else:
+            channel = self.channel
+            ylabel = f"{type} (summed up)"
 
         for i, file in enumerate(os.listdir(path)):
 
@@ -240,26 +199,16 @@ class DataProcessor:
 
     def threeD(self, type, bottom, top):
         
-        if type == "MIE":
-            path = self.path_MIE
-            os.makedirs(fr'{path}_3D', exist_ok=True)
-            channel = self.channel
-            channel_range = range(channel)
-        elif type == "DI":
-            path = self.path_DI
-            os.makedirs(fr'{path}_3D', exist_ok=True)
-            channel = self.channel
-            channel_range = range(channel)
-        elif type == "Mean":
-            path = self.path_mean
-            os.makedirs(fr'{path}_3D', exist_ok=True)
-            channel = self.channel
-            channel_range = range(channel)
-        elif type == "Minus":
-            path = self.path_minus
-            os.makedirs(fr'{path}_3D', exist_ok=True)
+        if type in ['Minus_MIE', 'Minus_DI', 'Minus_MM']:
             channel = self.channel - 1
             channel_range = range(1, channel+1)
+        else:
+            channel = self.channel
+            channel_range = range(channel)
+
+        path = getattr(self, f'path_{type}')
+        os.makedirs(fr'{path}_3D', exist_ok=True)
+            
 
         # 創建一個meshgrid
         x, y = np.meshgrid(range(1, self.scale+1), channel_range)
@@ -314,35 +263,34 @@ class DataProcessor:
 #     # TCUBA6.sum_up("Minus")
 #     # TCUBA6.histogram("Minus", -20, 50)
 #     TCUBA6.threeD("Minus", -2, 2)
-#     # TCUBA6.scatter_plot("Mean", -3, 3)
-#     # TCUBA6.sum_up("Mean")
-#     # TCUBA6.histogram("Mean", -25, 10)
-#     # TCUBA6.threeD("Mean")
+#     # TCUBA6.scatter_plot("MM", -3, 3)
+#     # TCUBA6.sum_up("MM")
+#     # TCUBA6.histogram("MM", -25, 10)
+#     # TCUBA6.threeD("MM")
 #     print(f'交大公教宿舍 finish{i}')
 
-for i in range(2,3):
-    NCREE = DataProcessor(f'七層鋼構架樓層破壞_一階差分_{i}_3_3', 8, i)
-    # NCREE.path()
-    # NCREE.calculate_DI()
-    # NCREE.calculate_MinusMean()
-     
-    # NCREE.scatter_plot("MIE", -5, 5)
-    # NCREE.scatter_plot("DI", -3, 3)
-    # NCREE.scatter_plot("Mean", -3, 3)
-    # NCREE.scatter_plot("Minus", -3, 3)
-    # NCREE.sum_up("MIE")
-    # NCREE.sum_up("DI")
-    # NCREE.sum_up("Mean")
-    # NCREE.sum_up("Minus")
-    # NCREE.histogram("MIE", -0, 100)
-    # NCREE.histogram("DI", -25, 50)
-    # NCREE.histogram("Mean", -25, 10)
-    # NCREE.histogram("Minus", -20, 50)
+for i in range(2, 16):
+    NCREE = DataProcessor(f'七層鋼構架樓層破壞_濾波_9601-13600_{i}_3_3', 8, i)
+    NCREE.calculate_DI()
+    NCREE.minus("MIE") 
     NCREE.minus("DI")  
+    NCREE.sum_up("MIE")
+    NCREE.sum_up("DI")
+    NCREE.sum_up("Minus_MIE")
+    NCREE.sum_up("Minus_DI")
+    NCREE.scatter_plot("MIE", -5, 5)
+    NCREE.scatter_plot("DI", -3, 3)
+    NCREE.scatter_plot("Minus_MIE", -3, 3)
+    NCREE.scatter_plot("Minus_DI", -3, 3)
+    NCREE.histogram("MIE", -0, 20)
+    NCREE.histogram("DI", -2, 2)
+    NCREE.histogram("Minus_MIE", -5, 5)
+    NCREE.histogram("Minus_DI", -2, 2)
     NCREE.threeD("MIE", 2.5, 5)
     NCREE.threeD("DI", -2, 2)
-    NCREE.threeD("Minus", -2, 2)
-    NCREE.threeD("Mean", -2, 2)
+    NCREE.threeD("Minus_MIE", -5, 5)
+    NCREE.threeD("Minus_DI", -2, 2)
+
     
-    print(f'七層鋼構架樓層破壞 finish{i}')
+    print(f'七層鋼構架樓層破壞_濾波 finish{i}')
 
